30ca055d6ce3b1598125b7c8e33c473da431dc1f
agatan <agatan039@gmail.com>
2017-07-01 14:28:56 +0900

    Add doccomment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! `signal-notify` crate provides a simple way to wait for signals in *nix systems through standard
+//! `std::sync::mpsc` API.
+//!
+//! ```no_run
+//! use signal_notify::{notify, Signal};
+//!
+//! let rx = notify(&[Signal::INT, Signal::HUP]);
+//! // block unitl receiving SIGINT or SIGHUP.
+//! // recv always return Ok because the sender channel will be never closed.
+//! rx.recv().unwrap()
+//! ```
+//!
+//! `signal-notify` doesn't support Windows. I'm not familiar with Windows, so I'd be happy if you
+//! could help me about it.
+
 extern crate libc;
 #[macro_use]
 extern crate lazy_static;
